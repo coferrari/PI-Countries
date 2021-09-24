f881043130fceb0edfbf8fdfcbffcf59223cd93d
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-09-24 20:54:52 -0300

    final update

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -4,7 +4,9 @@ import style from "./NavBar.module.css";
 const NavBar = () => {
   return (
     <div className={style.navbar}>
-        <h1 className={style.title}>countries<span className={style.titleSpan}>.</span></h1>
+      <h1 className={style.title}>
+        countries<span className={style.titleSpan}>.</span>
+      </h1>
     </div>
   );
 };
